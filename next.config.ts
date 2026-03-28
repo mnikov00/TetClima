5b7dffb688ee986f8e67e43b974f647aedb8240d
Martin Nikov <149877218+mnikov00@users.noreply.github.com>
2026-03-28 23:45:52 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,39 @@
+// import type { NextConfig } from "next";
+
+// const nextConfig: NextConfig = {
+//   images: {
+//     remotePatterns: [
+//       { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+//       {
+//         protocol: "http",
+//         hostname: "localhost",
+//         port: "1337",
+//         pathname: "/uploads/**",
+//       },
+//     ],
+//   },
+// };
+
+// export default nextConfig;
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
     remotePatterns: [
-      { protocol: "https", hostname: "images.unsplash.com", pathname: "/**" },
+      {
+        protocol: "https",
+        hostname: "images.unsplash.com",
+        pathname: "/**",
+      },
       {
         protocol: "http",
         hostname: "localhost",
         port: "1337",
+        pathname: "/uploads/**",
+      },
+      {
+        protocol: "https",
+        hostname: "tetclima-api.onrender.com",
         pathname: "/uploads/**",
       },
     ],
